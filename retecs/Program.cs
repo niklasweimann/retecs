d23c0950e3ae6a4e95307018b62fafef7b7b57ac
Niklas Weimann <nikweimann@gmail.com>
2022-07-21 22:11:02 +0200

    Update to .net6

--- retecs/Program.cs
+++ retecs/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using retecs.BlazorServices;
@@ -14,13 +15,14 @@ namespace retecs
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(
                 _ => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
             builder.Services.AddSingleton(SingletonEmitter.Instance);
             builder.Services.AddSingleton(new ConnectionService());
             builder.Services.AddSingleton<BrowserService>();
-            
+
             await builder.Build().RunAsync();
         }
     }
